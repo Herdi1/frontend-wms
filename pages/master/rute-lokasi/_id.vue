37d05a0c3c59748b3cbe11fa627bf1d6cadfb044
Herbid1 <hernaone1245@gmail.com>
2025-07-06 18:12:03 +0700

    rute pelanggan, rute lokasi

---
+++ pages/master/rute-lokasi/_id.vue
@@ -0,0 +1,408 @@
+<template>
+  <section class="section bg-white rounded-md px-4 py-2 shadow-sm">
+    <div class="section-body mb-4" v-if="!isLoadingPage">
+      <div class="flex justify-between items-center w-full">
+        <h1 v-if="isEditable" class="text-xl font-bold mb-2 uppercase">
+          Edit Data Master Rute Lokasi
+        </h1>
+        <h1 v-else class="text-xl font-bold mb-2 uppercase">
+          Tambah Data Master Rute Lokasi
+        </h1>
+        <button class="btn btn-primary my-2" @click="$router.back()">
+          <i class="fas fa-arrow-left mr-2"></i>
+          Kembali
+        </button>
+      </div>
+      <ValidationObserver v-slot="{ invalid, validate }" ref="formValidate">
+        <form
+          @submit.prevent="validate().then(() => onSubmit(invalid))"
+          autocomplete="off"
+        >
+          <div>
+            <label for="gudang_id"
+              >Gudang<span class="text-danger">*</span></label
+            >
+            <v-select
+              label="nama_gudang"
+              :loading="isLoadingGetGudang"
+              :options="lookup_location.data"
+              :filterable="false"
+              @search="onGetGudang"
+              v-model="parameters.form.gudang_id"
+              :reduce="(item) => item.gudang_id"
+              class="w-full mb-2"
+            >
+              <li
+                slot-scope="{ search }"
+                slot="list-footer"
+                class="p-1 border-t flex justify-between"
+                v-if="lookup_location.data.length || search"
+              >
+                <span
+                  v-if="lookup_location.current_page > 1"
+                  @click="onGetGudang(search, false)"
+                  class="flex-fill bg-primary text-white text-center cursor-pointer p-2 rounded"
+                  >Sebelumnya</span
+                >
+                <span
+                  v-if="
+                    lookup_location.last_page > lookup_location.current_page
+                  "
+                  @click="onGetGudang(search, true)"
+                  class="flex-fill bg-primary text-white text-center cursor-pointer p-2 rounded"
+                  >Selanjutnya</span
+                >
+              </li>
+            </v-select>
+          </div>
+
+          <div>
+            <label for="lokasi_id_awal"
+              >Lokasi Awal<span class="text-danger">*</span></label
+            >
+            <v-select
+              label="nama_lokasi"
+              :loading="isLoadingGeLokasiAwal"
+              :options="lookup_custom1.data"
+              :filterable="false"
+              @search="onGetLokasiAwal"
+              v-model="parameters.form.lokasi_id_awal"
+              :reduce="(item) => item.lokasi_id"
+              class="w-full mb-2"
+            >
+              <li
+                slot-scope="{ search }"
+                slot="list-footer"
+                class="p-1 border-t flex justify-between"
+                v-if="lookup_custom1.data.length || search"
+              >
+                <span
+                  v-if="lookup_custom1.current_page > 1"
+                  @click="onGetLokasiAwal(search, false)"
+                  class="flex-fill bg-primary text-white text-center cursor-pointer p-2 rounded"
+                  >Sebelumnya</span
+                >
+                <span
+                  v-if="lookup_custom1.last_page > lookup_custom1.current_page"
+                  @click="onGetLokasiAwal(search, true)"
+                  class="flex-fill bg-primary text-white text-center cursor-pointer p-2 rounded"
+                  >Selanjutnya</span
+                >
+              </li>
+            </v-select>
+          </div>
+
+          <div>
+            <label for="lokasi_id_tujuan"
+              >Lokasi Tujuan<span class="text-danger">*</span></label
+            >
+            <v-select
+              label="nama_lokasi"
+              :loading="isLoadingGetLokasiTujuan"
+              :options="lookup_defects.data"
+              :filterable="false"
+              @search="onGetLokasiTujuan"
+              v-model="parameters.form.lokasi_id_tujuan"
+              :reduce="(item) => item.lokasi_id"
+              class="w-full mb-2"
+            >
+              <li
+                slot-scope="{ search }"
+                slot="list-footer"
+                class="p-1 border-t flex justify-between"
+                v-if="lookup_defects.data.length || search"
+              >
+                <span
+                  v-if="lookup_defects.current_page > 1"
+                  @click="onGetLokasiTujuan(search, false)"
+                  class="flex-fill bg-primary text-white text-center cursor-pointer p-2 rounded"
+                  >Sebelumnya</span
+                >
+                <span
+                  v-if="lookup_defects.last_page > lookup_defects.current_page"
+                  @click="onGetLokasiTujuan(search, true)"
+                  class="flex-fill bg-primary text-white text-center cursor-pointer p-2 rounded"
+                  >Selanjutnya</span
+                >
+              </li>
+            </v-select>
+          </div>
+
+          <div class="form-group">
+            <input-form
+              label="Jarak"
+              type="text"
+              name="jarak"
+              v-model="parameters.form.jarak"
+              :required="false"
+            />
+          </div>
+
+          <div class="form-group">
+            <label for="status_approve"
+              >Status Approve<span class="text-danger">*</span></label
+            >
+            <select
+              class="mb-2 w-full pl-2 py-1 border rounded focus:outline-none"
+              name="status_approve"
+              id="status_approve"
+              v-model="parameters.form.status_approve"
+            >
+              <option value="">Pilih</option>
+              <option value="a">Approve</option>
+              <option value="p">Pending</option>
+            </select>
+          </div>
+
+          <modal-footer-section
+            class="mt-5"
+            :isLoadingForm="isLoadingForm"
+            @reset="formReset()"
+          />
+        </form>
+      </ValidationObserver>
+    </div>
+  </section>
+</template>
+
+<script>
+import { ValidationObserver, ValidationProvider } from "vee-validate";
+import { mapActions, mapState } from "vuex";
+export default {
+  props: ["self"],
+
+  data() {
+    let id = parseInt(this.$route.params.id);
+    return {
+      id,
+      isEditable: Number.isInteger(id) ? true : false,
+      isLoadingPage: Number.isInteger(id) ? true : false,
+      isLoadingForm: false,
+      title: "Master Rute Lokasi",
+
+      isStopSearchLokasiAwal: false,
+      isLoadingGeLokasiAwal: false,
+      lokasi_awal_search: "",
+      isStopSearchGudang: false,
+      isLoadingGetGudang: false,
+      gudang_search: "",
+      isStopSearchLokasiTujuan: false,
+      isLoadingGetLokasiTujuan: false,
+      lokasi_awal_search: "",
+
+      parameters: {
+        url: "master/rute-lokasi",
+        form: {
+          jarak: "",
+          status_approve: "p",
+          lokasi_id_awal: "",
+          lokasi_id_tujuan: "",
+          gudang_id: "",
+        },
+      },
+    };
+  },
+
+  async created() {
+    try {
+      if (this.isEditable) {
+        let res = await this.$axios.get(`master/rute-lokasi/${this.id}`);
+        this.parameters.form = res.data;
+        this.isLoadingPage = false;
+      }
+    } catch (error) {
+      this.$router.back();
+    }
+  },
+
+  async mounted() {
+    await this.onSearchLokasiAwal();
+    await this.onSearchGudang();
+    await this.onSearchLokasiTujuan();
+  },
+
+  computed: {
+    ...mapState("moduleApi", [
+      "error",
+      "result",
+      "lookup_location",
+      "lookup_defects",
+      "lookup_custom1",
+    ]),
+  },
+
+  methods: {
+    ...mapActions("moduleApi", ["addData", "updateData", "lookUp"]),
+
+    async onSubmit(isInvalid) {
+      if (isInvalid || this.isLoadingForm) return;
+
+      this.isLoadingForm = true;
+
+      let parameters = {
+        ...this.parameters,
+        form: {
+          ...this.parameters.form,
+          id: this.parameters.form.master_rute_lokasi_id
+            ? this.parameters.form.master_rute_lokasi_id
+            : "",
+        },
+      };
+
+      if (this.isEditable) {
+        await this.updateData(parameters);
+      } else {
+        await this.addData(parameters);
+      }
+
+      if (this.result == true) {
+        this.$toaster.success(
+          "Data berhasil di " + (this.isEditable == true ? "Diedit" : "Tambah")
+        );
+        this.isEditable = false;
+        this.parameters.form = {
+          jarak: "",
+          status_approve: "p",
+          lokasi_id_awal: "",
+          lokasi_id_tujuan: "",
+          gudang_id: "",
+        };
+        this.$refs.formValidate.reset();
+        this.$router.back();
+      } else {
+        this.$globalErrorToaster(this.$toaster, this.error);
+      }
+      this.isLoadingForm = false;
+    },
+
+    formReset() {
+      this.isEditable = false;
+      this.parameters.form = {
+        jarak: "",
+        status_approve: "p",
+        lokasi_id_awal: "",
+        lokasi_id_tujuan: "",
+        gudang_id: "",
+      };
+    },
+
+    //jenis peralatan
+    onGetLokasiAwal(search, isNext) {
+      if (!search.length && typeof isNext === "function") return false;
+
+      clearTimeout(this.isStopSearchLokasiAwal);
+
+      this.isStopSearchLokasiAwal = setTimeout(() => {
+        this.lokasi_awal_search = search;
+
+        if (typeof isNext !== "function") {
+          this.lookup_custom1.current_page = isNext
+            ? this.lookup_custom1.current_page + 1
+            : this.lookup_custom1.current_page - 1;
+        } else {
+          this.lookup_custom1.current_page = 1;
+        }
+
+        this.onSearchLokasiAwal();
+      }, 600);
+    },
+
+    async onSearchLokasiAwal() {
+      if (!this.isLoadingGeLokasiAwal) {
+        this.isLoadingGeLokasiAwal = true;
+
+        await this.lookUp({
+          url: "master/lokasi/get-lokasi",
+          lookup: "custom1",
+          query:
+            "?search=" +
+            this.lokasi_awal_search +
+            "&page=" +
+            this.lookup_custom1.current_page +
+            "&per_page=10",
+        });
+
+        this.isLoadingGeLokasiAwal = false;
+      }
+    },
+    //gudang
+    onGetGudang(search, isNext) {
+      if (!search.length && typeof isNext === "function") return false;
+
+      clearTimeout(this.isStopSearchGudang);
+
+      this.isStopSearchGudang = setTimeout(() => {
+        this.gudang_search = search;
+
+        if (typeof isNext !== "function") {
+          this.lookup_location.current_page = isNext
+            ? this.lookup_location.current_page + 1
+            : this.lookup_location.current_page - 1;
+        } else {
+          this.lookup_location.current_page = 1;
+        }
+
+        this.onSearchGudang();
+      }, 600);
+    },
+
+    async onSearchGudang() {
+      if (!this.isLoadingGetGudang) {
+        this.isLoadingGetGudang = true;
+
+        await this.lookUp({
+          url: "master/gudang/get-gudang",
+          lookup: "location",
+          query:
+            "?search=" +
+            this.gudang_search +
+            "&page=" +
+            this.lookup_location.current_page +
+            "&per_page=10",
+        });
+
+        this.isLoadingGetGudang = false;
+      }
+    },
+
+    onGetLokasiTujuan(search, isNext) {
+      if (!search.length && typeof isNext === "function") return false;
+
+      clearTimeout(this.isStopSearchLokasiTujuan);
+
+      this.isStopSearchLokasiTujuan = setTimeout(() => {
+        this.lokasi_awal_search = search;
+
+        if (typeof isNext !== "function") {
+          this.lookup_defects.current_page = isNext
+            ? this.lookup_defects.current_page + 1
+            : this.lookup_defects.current_page - 1;
+        } else {
+          this.lookup_defects.current_page = 1;
+        }
+
+        this.onSearchLokasiTujuan();
+      }, 600);
+    },
+
+    async onSearchLokasiTujuan() {
+      if (!this.isLoadingGetLokasiTujuan) {
+        this.isLoadingGetLokasiTujuan = true;
+
+        await this.lookUp({
+          url: "master/lokasi/get-lokasi",
+          lookup: "defects",
+          query:
+            "?search=" +
+            this.lokasi_awal_search +
+            "&page=" +
+            this.lookup_defects.current_page +
+            "&per_page=10",
+        });
+
+        this.isLoadingGetLokasiTujuan = false;
+      }
+    },
+  },
+};
+</script>
---
+++ pages/master/rute-lokasi/index.vue
@@ -0,0 +1,311 @@
+<template>
+  <section>
+    <ul class="flex space-x-2 rtl:space-x-reverse mb-5">
+      <li>
+        <a href="javascript:;" class="text-primary hover:underline">Setting</a>
+      </li>
+      <li
+        class="relative pl-4 before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:content-['/'] before:text-gray-400"
+      >
+        <span>Rute Lokasi</span>
+      </li>
+    </ul>
+    <div class="mb-5 flex items-center justify-between">
+      <h5 class="text-lg font-semibold dark:text-white-light">
+        {{ this.title }}
+      </h5>
+    </div>
+    <div class="flex sm:flex-col md:flex-row gap-5">
+      <div class="w-full bg-white rounded-md p-2 px-4">
+        <div>
+          <list-option-section :self="this" ref="form-option" />
+        </div>
+        <div>
+          <table ref="formContainer">
+            <thead>
+              <tr class="uppercase">
+                <th class="w-[5%] text-center">Edit</th>
+                <th class="w-[5%] text-center">Delete</th>
+                <th class="w-[5%]">No</th>
+                <th>Gudang</th>
+                <th>Lokasi Awal</th>
+                <th>Lokasi Tujuan</th>
+                <th>Jarak</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, i) in data" :key="i">
+                <td>
+                  <small-edit-button @click="onEdit(item)" />
+                </td>
+                <td>
+                  <small-delete-button
+                    @click="onTrashed(item)"
+                    v-if="!item.deleted_at"
+                  />
+                </td>
+                <td>
+                  {{
+                    (parameters.params.page - 1) * parameters.params.per_page +
+                    i +
+                    1
+                  }}
+                </td>
+                <td>
+                  {{
+                    item.gudang ? item.gudang.nama_gudang : "Tidak Ditemukan"
+                  }}
+                </td>
+                <td>
+                  {{
+                    item.lokasi_awal
+                      ? item.lokasi_awal.nama_lokasi
+                      : "Tidak Ditemukan"
+                  }}
+                </td>
+                <td>
+                  {{
+                    item.lokasi_tujuan
+                      ? item.lokasi_tujuan.nama_lokasi
+                      : "Tidak Ditemukan"
+                  }}
+                </td>
+                <td>{{ item.jarak }}</td>
+                <td>
+                  {{ item.status_approve == "a" ? "Approved" : "Pending" }}
+                </td>
+              </tr>
+            </tbody>
+            <table-data-loading-section :self="this" />
+
+            <table-data-not-found-section :self="this" />
+          </table>
+        </div>
+        <div class="mx-3 mt-2 mb-4">
+          <pagination-section :self="this" ref="pagination" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapActions, mapState, mapMutations } from "vuex";
+export default {
+  middleware: ["checkRoleUser"],
+
+  head() {
+    return {
+      title: "Master Rute Lokasi",
+    };
+  },
+
+  created() {
+    this.set_data([]);
+    this.onLoad();
+  },
+
+  mounted() {
+    this.$refs["form-option"].isMaintenancePage = false;
+    this.$refs["form-option"].isExport = false;
+    this.$refs["form-option"].isFilter = false;
+    this.$refs["form-option"].isAddData = true;
+    if (
+      this.getRoles.destroy ||
+      this.getRoles.destroy_all ||
+      this.getRoles.restore ||
+      this.getRoles.restore_all
+    ) {
+      this.$refs["form-option"].isMaintenancePage = false;
+    }
+
+    if (this.getRoles.store) {
+      this.$refs["form-option"].isAddData = true;
+    }
+
+    if (this.getRoles.export) {
+      this.$refs["form-option"].isExportFile = true;
+
+      this.$refs["form-option"].isExportFilePdf = true;
+      this.$refs["form-option"].isExportFileExcel = true;
+
+      if ("export_pdf" in this.getRoles || "export_excel" in this.getRoles) {
+        this.$refs["form-option"].isExportFilePdf = this.getRoles.export_pdf;
+        this.$refs["form-option"].isExportFileExcel =
+          this.getRoles.export_excel;
+      }
+    }
+
+    if (this.getRoles.print) {
+      this.$refs["form-option"].isExportPrint = true;
+    }
+  },
+
+  computed: {
+    ...mapState("moduleApi", ["data", "error", "result"]),
+
+    getRoles() {
+      if (this.user.is_superadmin == 1) {
+        return this.default_roles;
+      } else {
+        let main_role = this.user.role.menus.find(
+          (item) => item.rute == "rute-lokasi"
+        );
+
+        let roles = {};
+
+        if (JSON.parse(main_role.pivot.operators).includes("all")) {
+          return this.default_roles;
+        }
+
+        JSON.parse(main_role.pivot.operators).forEach((item) => {
+          roles[item.replace("-", "_")] = true;
+        });
+
+        return roles;
+      }
+    },
+  },
+
+  data() {
+    return {
+      title: "Master Rute Lokasi",
+      isLoadingData: false,
+      isPaginate: true,
+      user: this.$auth.user,
+      default_roles: {
+        store: true,
+        update: true,
+        destroy: true,
+        restore: true,
+        show: true,
+        export: true,
+        export_excel: true,
+        export_pdf: true,
+        print: true,
+        destroy_all: true,
+        restore_all: true,
+        import: true,
+      },
+      parameters: {
+        url: "master/rute-lokasi",
+        type: "pdf",
+        params: {
+          soft_deleted: "",
+          search: "",
+          order: "master_rute_lokasi_id",
+          sort: "desc",
+          all: "",
+          per_page: 10,
+          page: 1,
+          form: {
+            gudang_id: "",
+            lokasi_awal_id: "",
+            lokasi_tujuan_id: "",
+            jarak: "",
+            status_approve: "",
+          },
+        },
+        loadings: {
+          isDelete: false,
+          isRestore: false,
+        },
+      },
+    };
+  },
+
+  methods: {
+    ...mapActions("moduleApi", [
+      "getData",
+      "deleteData",
+      "restoreData",
+      "deleteAllData",
+      "restoreAllData",
+    ]),
+    ...mapMutations("moduleApi", ["set_data"]),
+
+    async onLoad(page = 1) {
+      if (this.isLoadingData) return;
+
+      this.isLoadingData = true;
+      this.parameters.params.page = page;
+
+      let loader = this.$loading.show({
+        container: this.$refs.formContainer,
+        canCancel: true,
+        onCancel: this.onCancel,
+      });
+
+      await this.getData(this.parameters);
+
+      if (this.result == true) {
+        loader.hide();
+
+        if (page == 1) {
+          this.$refs["pagination"].generatePage();
+        }
+
+        this.$refs["pagination"].active_page = this.parameters.params.page;
+      } else {
+        this.$globalErrorToaster(this.$toaster, this.error);
+      }
+
+      this.isLoadingData = false;
+    },
+
+    onFormShow() {
+      this.$router.push("/master/rute-lokasi/add");
+    },
+
+    onEdit(item) {
+      this.$router.push("/master/rute-lokasi/" + item.master_rute_lokasi_id);
+    },
+
+    onTrashed(item) {
+      if (this.parameters.loadings.isDelete) return;
+
+      this.$confirm({
+        auth: false,
+        message: "Data ini akan dipindahkan ke dalam Trash. Yakin ??",
+        button: {
+          no: "No",
+          yes: "Yes",
+        },
+        callback: async (confirm) => {
+          if (confirm) {
+            this.parameters.loadings.isDelete = true;
+
+            await this.deleteData({
+              url: this.parameters.url,
+              id: item.master_rute_lokasi_id,
+              params: this.parameters.params,
+            });
+
+            if (this.result == true) {
+              this.onLoad(this.parameters.params.page);
+              this.$toaster.success(
+                "Data berhasil di pindahkan ke dalam Trash!"
+              );
+            } else {
+              this.$globalErrorToaster(this.$toaster, this.error);
+            }
+
+            this.parameters.loadings.isDelete = false;
+          }
+        },
+      });
+    },
+
+    onSort(column, sort = "asc") {
+      this.parameters.params = {
+        ...this.parameters.params,
+        order: column,
+        sort: sort,
+      };
+
+      this.onLoad(this.parameters.params.page);
+    },
+  },
+};
+</script>
