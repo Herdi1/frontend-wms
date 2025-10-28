58e4eda9e1938733c89221574186166c3b6ad63f
Hernawan Abidin <97391283+Herdi1@users.noreply.github.com>
2025-10-28 14:20:03 +0700

    build

--- static/sw.js
+++ static/sw.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 // THIS FILE SHOULD NOT BE VERSION CONTROLLED
-=======
-const options = {"workboxURL":"https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.4/workbox/workbox-sw.js","importScripts":[],"config":{"debug":false},"cacheOptions":{"cacheId":"client-prod","directoryIndex":"/","revision":"RQqBgR150whm"},"clientsClaim":true,"skipWaiting":true,"cleanupOutdatedCaches":true,"offlineAnalytics":false,"preCaching":[{"revision":"RQqBgR150whm","url":"/?standalone=true"}],"runtimeCaching":[{"urlPattern":"/_nuxt/","handler":"CacheFirst","method":"GET","strategyPlugins":[]},{"urlPattern":"/","handler":"NetworkFirst","method":"GET","strategyPlugins":[]}],"offlinePage":null,"pagesURLPattern":"/","offlineStrategy":"NetworkFirst"}
->>>>>>> bc6f2cdaad5b7a9d05b417ef5635ee7ada334fac
 
 // https://github.com/NekR/self-destroying-sw
 
